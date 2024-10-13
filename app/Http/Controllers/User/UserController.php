65763c3921eea7fbf7bcc1ab7397b7ffde46a801
Frank Cairampoma <ingfralch@gmail.com>
2024-10-13 07:53:08 -0500

    Primer commit: Subiendo todo el proyecto

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('web'); // Habilita el middleware web que maneja las sesiones
+    }
+
+    /**
+     * Método para iniciar sesión y crear la sesión del usuario.
+     */
+    public function create(Request $request): Response
+    {
+        // Validar los datos de entrada
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        // Crear el usuario
+        $user = User::create([
+            'name' => $request->name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+            'remember_token' => "token",
+            'is_paid_user' => $request->is_paid_user,
+            'role' => $request->role
+        ]);
+
+
+        return   response([
+            'message' => 'User created successfully',
+            'user' => $user
+        ]); 
+    }
+
+    public function login(Request $request)
+    {
+        // Validar los datos de entrada
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Buscar el usuario por email
+        $user = User::where('email', $request->email)->first();
+
+        // Verificar si el usuario existe y si la contraseña es correcta
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'These credentials do not match our records.'
+            ], 404);
+        }
+
+        // Guardar la sesión del usuario
+        session(['user' => $user]);
+
+        return response([
+            'message' => 'Login successful',
+            'user' => $user
+        ], 200);
+    }
+
+    /**
+     * Método para obtener la sesión del usuario.
+     */
+    public function getSession()
+    {
+        // Obtener toda la sesión
+        $sessionData = session()->all();
+
+        return response()->json($sessionData, 200);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function all()
+    {
+        $users = User::all();
+        return $users;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/user/create', [UserController::class, 'create']);
+
+Route::post('/user/login', [UserController::class, 'login']);
+
+Route::get('/user/getSession', [UserController::class, 'getSession']);
+
+Route::get('/user/all', [UserController::class, 'all']);
+
+Route::get('/user/{id}', [UserController::class, 'show']);
+
