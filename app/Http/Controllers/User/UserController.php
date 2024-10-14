db1bc7ba1d4649c393a691ad951e54a0f8f4b3de
Fralch <ingfralch@gmail.com>
2024-10-14 18:31:13 -0500

    estatus login

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -61,7 +61,7 @@ class UserController extends Controller
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
                 'message' => 'These credentials do not match our records.'
-            ], 404);
+            ], 200);
         }
 
         // Guardar la sesión del usuario
