abd1150554e9488af8fd6bd634bc13b8c1be709f
Frank Cairampoma <ingfralch@gmail.com>
2024-10-20 08:00:47 -0500

    upload file

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -20,32 +20,41 @@ class UserController extends Controller
      * Método para iniciar sesión y crear la sesión del usuario.
      */
     public function create(Request $request): Response
-    {
-        // Validar los datos de entrada
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'string', 'min:8'],
-        ]);
+{
+    // Validar los datos de entrada
+    $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'last_name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users'], // Asegúrate de que 'users' sea el nombre correcto de la tabla
+        'password' => ['required', 'string', 'min:8'],
+        'profile_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'], // Validación para la imagen (opcional)
+    ]);
 
-        // Crear el usuario
-        $user = User::create([
-            'name' => $request->name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'password' => Hash::make($request->string('password')),
-            'remember_token' => "token",
-            'is_paid_user' => $request->is_paid_user,
-            'role' => $request->role
-        ]);
-
-
-        return   response([
-            'message' => 'User created successfully',
-            'user' => $user
-        ]);
+    // Procesar la imagen si se proporciona
+    $profileImagePath = null;
+    if ($request->hasFile('profile_image')) {
+        $image = $request->file('profile_image');
+        $profileImagePath = time() . '.' . $image->getClientOriginalExtension();
+        $image->move(public_path('images/profile'), $profileImagePath); // Guardar la imagen en public/images/profile
     }
 
+    // Crear el usuario
+    $user = User::create([
+        'name' => $request->name,
+        'last_name' => $request->last_name,
+        'email' => $request->email,
+        'password' => Hash::make($request->input('password')),
+        'is_paid_user' => 0,
+        'role' => $request->input('role', 'user'), // Valor por defecto es 'user' si no se pasa
+        'uri' => $profileImagePath || '', // Almacena la ruta de la imagen si existe
+    ]);
+
+    // Retornar la respuesta con los datos del usuario
+    return response([
+        'message' => 'User created successfully',
+        'user' => $user
+    ], 201);
+}
     public function login(Request $request)
     {
         // Validar los datos de entrada
