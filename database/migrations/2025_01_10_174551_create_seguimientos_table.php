ba089915a681c1bc25186a63aaf284e1f2002002
Fralch <ingfralch@gmail.com>
2025-01-12 10:13:10 -0500

    Enhance migration for 'seguimientos' table with additional comments and TODO notes for future improvements

--- database/migrations/2025_01_10_174551_create_seguimientos_table.php
+++ database/migrations/2025_01_10_174551_create_seguimientos_table.php
@@ -11,7 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('seguimientos', function (Blueprint $table) {
+        /* 
+         LOS PROJECTOS: tiene seguimientos
+         LOS SEGUIMIENTOS: tienen semanas
+         LAS SEMANAS: tienen dias
+         LAS DIAS: tienen actividades
+        */
+
+        /*
+            TODO: calcular bien los dias en la semana y que comiencen los dias lunes de cada semana 
+         */
+        Schema::create('seguimientos', function (Blueprint $table) { 
             $table->id();
             $table->foreignId('dia_id')->constrained('dias')->onDelete('cascade');
             $table->string('titulo');
