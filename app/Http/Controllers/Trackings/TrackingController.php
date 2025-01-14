555201a295f786a6688005330337c49536ee40a9
Fralch <ingfralch@gmail.com>
2025-01-14 17:09:47 -0500

    Remove commented-out migration schema from Tracking model to clean up code and improve readability.

---
+++ app/Http/Controllers/Trackings/TrackingController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Trackings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tracking;
+use App\Models\Day;
+use App\Models\Week;
+use App\Models\Project;
+use App\Models\User;
+use App\Models\Activity;
+
+
+class TrackingController extends Controller
+{
+    public function trackingAll(){
+        $trackings = Tracking::all();
+        return response()->json($trackings);
+    }
+
+    public function trackingByWeekByProject($week_id, $project_id){
+        $trackings = Tracking::where('week_id', $week_id)->where('project_id', $project_id)->get();
+        return response()->json($trackings);
+    }
+
+
+    
+   
+
+    // crear tracking
+    public function createTracking(Request $request){
+        
+        $project_id = $request->project_id;
+        $user_id = $request->user_id;
+        $title = $request->title;
+        $description = $request->description;
+
+        // obtener las semanas de un proyecto
+        $weeks = Week::where('project_id', $project_id)->get();
+
+        // crear los trackings de las semanas
+        foreach($weeks as $week){
+            Tracking::create([
+                'week_id' => $week->id,
+                'project_id' => $project_id,
+                'user_id' => $user_id,
+                'title' => $title,
+                'description' => $description,
+                'date' => now(),
+            ]);
+        }
+        return response()->json(['message' => 'Tracking creado correctamente']);
+    }
+}
+
+
--- app/Models/Tracking.php
+++ app/Models/Tracking.php
@@ -9,24 +9,7 @@ class Tracking extends Model
 {
     use HasFactory;
 
-    /*
-    Schema::create('trackings', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('week_id');
-            $table->unsignedBigInteger('project_id');
-            $table->unsignedBigInteger('user_id');
-            $table->string('title'); 
-            $table->string('description')->nullable();
-            $table->date('date')->default(now());   
-            $table->boolean('status')->default(true); // Estado del seguimiento
-            $table->timestamps();
-
-            $table->foreign('week_id')->references('id')->on('weeks')->onDelete('cascade');
-            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
-
-     */
+  
     protected $fillable = [
         'week_id',
         'project_id',
