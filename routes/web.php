7b471918bc3899ab7c24af946dc80c569a813578
Fralch <ingfralch@gmail.com>
2025-04-08 19:39:10 -0500

    Update project update route to include project ID as a parameter for better resource management.

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::post('/endpoint/project/subtype/store', [ProjectSubtypeController::class,
 // Projects
 Route::get('/endpoint/projects', [ProjectController::class, 'all']);
 Route::post('/endpoint/project/store', [ProjectController::class, 'store']);
-Route::post('/endpoint/project/update', [ProjectController::class, 'updateProject']);
+Route::post('/endpoint/project/update/{id}', [ProjectController::class, 'updateProject']);
 Route::delete('/endpoint/project/destroy/{id}', [ProjectController::class, 'destroyProject']);
 
 // Projects entities
