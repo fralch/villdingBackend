7d68bdbb3ac3454f1f00bd2c966bd573ee812a5e
Fralch <ingfralch@gmail.com>
2025-04-13 18:50:31 -0500

    (routes): rename activity completion endpoint for consistency

--- routes/web.php
+++ routes/web.php
@@ -143,5 +143,5 @@ Route::get('/endpoint/activities/project/{project_id}', [ActivityController::cla
 Route::get('/endpoint/activities/tracking/{tracking_id}', [ActivityController::class, 'activityByTracking']);
 Route::post('/endpoint/activities/{id}', [ActivityController::class, 'updateActivity']);
 Route::post('/endpoint/activities_imgs/{id}', [ActivityController::class, 'updateActivityWithImages']);
-Route::post('/endpoint/activities/complete', [ActivityController::class, 'completeActivity']);
+Route::post('/endpoint/activities_complete', [ActivityController::class, 'completeActivity']);
 Route::delete('/endpoint/activities/{id}', [ActivityController::class, 'deleteActivity']);
